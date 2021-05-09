5fa287509f3e2f636953b4aef71deb214e52240d
Dimitry Goldenberg <epuckop@gmail.com>
2021-05-09 23:33:50 +0300

    creating firs file :)

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Telegram Bot made by stupid me :)
+"""
+
+# Imports
+
+
+# Theatrical decoration and preperation
+def main():
+    print("Hello World!")
+
+# Lets the show begun
+if __name__ == "__main__":
+    main()
